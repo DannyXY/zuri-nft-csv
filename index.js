0ccd8eebac142b0ce4fb270f1d1ee3edf23f0803
Adewambe Daniel <danoscolobo@gmail.com>
2022-11-05 20:50:35 +0100

    Fixed bugs

--- index.js
+++ index.js
@@ -9,17 +9,6 @@ const readline = require("readline").createInterface({
 
 require("dotenv").config({ path: ".env" });
 
-fs.writeFileSync(
-  "filename.output.csv",
-  "Serial Number,Name,UUID,Hash\n",
-  function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      return;
-    }
-  }
-);
 
 console.log(figlet.textSync("ZURI CSV PARSER"));
 console.log("Welcome to Zuri CSV Parser");
@@ -40,6 +29,17 @@ readline.question(
       .pipe(csv({}))
       .on("data", (data) => result.push(data))
       .on("end", () => {
+        fs.writeFileSync(
+          `${path.split(".")[0]}.output.csv`,
+          "Serial Number,Name,UUID,Hash\n",
+          function (err) {
+            if (err) {
+              console.log(err);
+            } else {
+              return;
+            }
+          }
+        );
         const formattedJson = [];
         // write the json output of the csv file into a json file in the root of the directory
         for (let i = 0; i < result.length; i++) {
@@ -88,7 +88,7 @@ readline.question(
           // Continuosly append a new line to the output.csv file for every entry
           // Filter out the ones with empty names(Team Serials) so their hash doesn't get passed back
           fs.appendFileSync(
-            "filename.output.csv",
+            `${path.split(".")[0]}.output.csv`,
             `${jsonObj["Series Number"]},${
               finalObject.collection.name ? finalObject.collection.name : ""
             },${jsonObj["UUID"] ? jsonObj["UUID"] : ""},${
@@ -106,87 +106,11 @@ readline.question(
 
         console.log("💚 Successfully parsed CSV into filename.output.csv");
         console.log("Parsing JSON file...");
-        fs.writeFileSync("filename.output.json", JSON.stringify(formattedJson));
+        fs.writeFileSync(
+          `${path.split(".")[0]}.output.json`,
+          JSON.stringify(formattedJson)
+        );
         console.log(`❤ Successfully parsed filename.output.json`);
       });
   }
 );
-
-
-// Without NPM package
-
-// console.log(array.length);
-// let result = [];
-
-// The array[0] contains all the
-// header columns so we store them
-// in headers array
-// let headers = array[0].split(",");
-// console.log(headers);
-// Since headers are separated, we
-// need to traverse remaining n-1 rows.
-// for (let i = 1; i < array.length - 1; i++) {
-//   let obj = {};
-
-//   // Create an empty object to later add
-//   // values of the current row to it
-//   // Declare string str as current array
-//   // value to change the delimiter and
-//   // store the generated string in a new
-//   // string s
-//   let str = array[i];
-
-//   let props = str.match(/(\s*'[^']+'|\s*[^,]+)(?=,|$)/g);
-//   const newProp = props.filter((item) => item !== ",");
-//   console.log(newProp);
-
-//   //   // For each header, if the value contains
-//   //   // multiple comma separated data, then we
-//   //   // store it in the form of array otherwise
-//   //   // directly the value is stored
-//   for (let j = 0; j < headers.length; j++) {
-//     let header = headers[j];
-//     obj[header] = newProp[j].trim();
-//   }
-
-// const formattedObj = {
-//   format: "CHIP-0007",
-//   name: "All team naming",
-//   description: obj["description"],
-//   minting_tool: "",
-//   sensitive_content: false,
-//   series_number: obj["Series Number"],
-//   series_total: 1000,
-//   attributes: [],
-//   collection: {
-//     name: obj["Filename"],
-//     id: obj["UUID"],
-//     attributes: [],
-//   },
-// };
-
-// const finalObject = {
-//   ...formattedObj,
-//   data: {
-//     hash: sha256(JSON.stringify(formattedObj) + "lolol"),
-//   },
-// };
-//   fs.appendFileSync(
-//     "output.csv",
-//     `${obj["Series Number"]},${finalObject.collection.name},${obj["UUID"]},${finalObject.data.hash}\n`,
-//     function (err) {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log("next");
-//       }
-//     }
-//   );
-//   console.log(obj);
-//   result.push(finalObject);
-// }
-
-// Convert the resultant array to json and
-// generate the JSON output file.
-// fs.writeFileSync("output.json", JSON.stringify(jsonObj));
-// console.log(result);
